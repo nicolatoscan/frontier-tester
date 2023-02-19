1b9840eb987bc606f5d0fc80a0b3ffe949f981ab
Nicola Toscan <nicolatoscan99@outlook.it>
2023-02-19 22:43:07 +0100

    exp 100000

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,25 @@ import FrontierTester, { FrontierTesterProps } from './Tester';
 
 const props: FrontierTesterProps[] = [
 
-  { n: 0,  numTuples: 20000, replicationFactor: 2, chainLength: 1, kill: false, rateLimitSrc: true,  maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
-  { n: 1,  numTuples: 20000, replicationFactor: 2, chainLength: 1, kill: false, rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
+  { n: 100,  numTuples: 100000, replicationFactor: 2, chainLength: 1, kill: false, rateLimitSrc: true,  maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
+  { n: 101,  numTuples: 100000, replicationFactor: 2, chainLength: 1, kill: false, rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
 
-  { n: 2,  numTuples: 20000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
-  { n: 3,  numTuples: 20000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
-  { n: 4,  numTuples: 20000, replicationFactor: 2, chainLength: 2, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
+  { n: 102,  numTuples: 100000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
+  { n: 103,  numTuples: 100000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
+  { n: 104,  numTuples: 100000, replicationFactor: 2, chainLength: 2, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
 
-  { n: 5,  numTuples: 20000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: true,  maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
-  { n: 6,  numTuples: 20000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: true,  maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
-  { n: 7,  numTuples: 20000, replicationFactor: 2, chainLength: 2, kill: true,  rateLimitSrc: true,  maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
+  { n: 105,  numTuples: 100000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: true,  maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
+  { n: 106,  numTuples: 100000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: true,  maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
+  { n: 107,  numTuples: 100000, replicationFactor: 2, chainLength: 2, kill: true,  rateLimitSrc: true,  maxSrcTotalQueueSizeTuples: 100,  maxTotalQueueSizeTuples: 100  },
   
-  { n: 8,  numTuples: 20000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 10,   maxTotalQueueSizeTuples: 10   },
-  { n: 9,  numTuples: 20000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 10,   maxTotalQueueSizeTuples: 10   },
+  { n: 108,  numTuples: 100000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 10,   maxTotalQueueSizeTuples: 10   },
+  { n: 109,  numTuples: 100000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 10,   maxTotalQueueSizeTuples: 10   },
   
-  { n: 10, numTuples: 20000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 1000, maxTotalQueueSizeTuples: 1000 },
-  { n: 11, numTuples: 20000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 1000, maxTotalQueueSizeTuples: 1000 },
+  { n: 110, numTuples: 100000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 1000, maxTotalQueueSizeTuples: 1000 },
+  { n: 111, numTuples: 100000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 1000, maxTotalQueueSizeTuples: 1000 },
   
-  { n: 12, numTuples: 20000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 5000, maxTotalQueueSizeTuples: 5000 },
-  { n: 13, numTuples: 20000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 5000, maxTotalQueueSizeTuples: 5000 },
+  { n: 112, numTuples: 100000, replicationFactor: 2, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 5000, maxTotalQueueSizeTuples: 5000 },
+  { n: 113, numTuples: 100000, replicationFactor: 3, chainLength: 1, kill: true,  rateLimitSrc: false, maxSrcTotalQueueSizeTuples: 5000, maxTotalQueueSizeTuples: 5000 },
 
 ]
 
